5be7f970323183dde3122f95f27739dc6c7fce7c
Sarathek <135970994+Sarathek@users.noreply.github.com>
2023-07-17 15:38:26 +0530

    Completed Q9

---
+++ test9.js
@@ -0,0 +1,18 @@
+let arr = ['react', 'html', 'edstm'];
+let str = 'edstem';
+f=true;
+
+for (let i = 0; i < arr.length; i++) {
+if (arr[i] === str)
+{
+
+for (let j=0;j<str;j++)
+if (a[i][j]!=str[j])
+
+f=false;
+}
+else
+f=false;
+}
+
+console.log(f);
